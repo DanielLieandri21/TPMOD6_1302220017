fa363c4188f2f6e3addb37f9769d65d65a603fe3
DanielLieandri21 <dlieandri@gmail.com>
2024-03-25 04:53:04 +0700

    implementasi design by contract

--- TPMOD6_1302220017/Program.cs
+++ TPMOD6_1302220017/Program.cs
@@ -74,4 +74,14 @@ public class SayaTubeVideo
         }
     }
 
+    static void Main(string[] args)
+    {
+        string judul = "Tutorial Design By Contract - Daniel Lieandri_1302220017";
+        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo(judul);
+        Random rand = new Random();
+        int min = 1;
+        int max = 10000000;
+        sayaTubeVideo.IncreasePlayCount(rand.Next(min, max + 1));
+        sayaTubeVideo.PrintVideoDetails();
+    }
 }
